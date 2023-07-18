315db9062d2e018f210b1640549e6759f98897a2
William Saimo <will.saimo@outlook.com>
2023-07-18 15:04:51 -0500

    added functionality for the patch_parser function. takes an image and gives an image without a background, but still needs to not be so scuffed

--- src.py
+++ src.py
@@ -1,4 +1,10 @@
 #imports
+from rembg import remove
+from PIL import Image
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
+import numpy as np
+
 
 def database_checker(picture):
     #will be run in patch_parser
@@ -7,12 +13,18 @@ def database_checker(picture):
     #either creates new patch in 'Patchadex' or is put in the same folder as a 'Patchadex' entry
     pass
 
-def patch_parser(path):
+def patch_parser(file_name, desired_file_name):
     #takes path of unparsed_data
     #all images of unparsed_data will have backgrounds removed and will ask for a name
     #all parsed_data will be put in the available folder
     #unparsed_data will be purged
-    pass
+    input_perfect = file_name
+    output_perfect = desired_file_name
+    input = Image.open(input_perfect)
+    print(input)
+    input.save(r'data/unparsed_data/20230718_091148.png')
+    output = remove(input)
+    output.save(output_perfect)
 
 def debugger():
     #unsure of how to implement, will ask for instruction later
@@ -22,4 +34,4 @@ def debugger():
 
 
 if __name__ == "__main__":
-    print("hello")
+    patch_parser('data/unparsed_data/20230718_091148.jpg', 'data/parsed_data/chimera_patch.png')
