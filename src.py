11c7d4d1c865381278c10908bdefbc4a03378374
William Saimo <will.saimo@outlook.com>
2023-07-19 12:29:05 -0500

    cleaned up the code a little bit, turned the photos into thumbnails for learning

--- src.py
+++ src.py
@@ -4,29 +4,24 @@ from PIL import Image
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
 import numpy as np
-
-
-def database_checker(picture):
-    #will be run in patch_parser
-    #takes a picture and asks database if it looks like any of the pictures within
-    #if not, looks at the name and does the same
-    #either creates new patch in 'Patchadex' or is put in the same folder as a 'Patchadex' entry
-    pass
+from duckduckgo_search import ddg_images
+from fastcore.all import *
+from time import sleep
+from fastdownload import download_url
+from fastai.vision.all import *
+from time import sleep
 
 def patch_parser(file_name, desired_file_name):
     #takes path of unparsed_data || this is complete
     #all images of unparsed_data will have backgrounds removed and will ask for a name
     #all parsed_data will be put in the available folder
     #unparsed_data will be purged - to be completed among last steps for testing purposes
-    input_perfect = file_name
-    output_perfect = desired_file_name
-    input = Image.open(input_perfect)
-    print(input)
-    input.save(r'data/unparsed_data/20230718_090942.png')
+    input = Image.open(file_name).to_thumb(254,254)
+    input.save(file_name)
     output = remove(input)
     imageBox = output.getbbox()
     output_boxed = output.crop(imageBox)
-    output_boxed.save(output_perfect)
+    output_boxed.save(desired_file_name)
 
 def debugger():
     #unsure of how to implement, will ask for instruction later
@@ -36,4 +31,4 @@ def debugger():
 
 
 if __name__ == "__main__":
-    patch_parser('data/unparsed_data/20230718_090942.jpg', 'data/parsed_data/chimera_patch.png')
+    patch_parser('data/unparsed_data/20230718_103348.jpg', 'data/parsed_data/chimera_patch.png')
